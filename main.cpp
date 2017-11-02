56745030f63efec725555f487a9c806be9adc4ae
Sebastian Mai <bastinat0r@bastinat0r.de>
2017-11-02 16:40:19 +0100

    adding dw1000 class to main function

--- main.cpp
+++ main.cpp
@@ -1,32 +1,23 @@
 #include "mbed.h"
-
+#include "dw1000_spi.h"
+ 
 PwmOut led1(LED1);
 DigitalOut led2(LED2);
-SPI spi(SPI_MISO, SPI_MOSI, SPI_SCK);
-DigitalOut cs(SPI_CS);
+SPI spi(SPI_MISO, SPI_MOSI, SPI_SCK, SPI_CS);
+DigitalIn sIRQ(PA_0);
+DigitalOut sReset(PA_1);
+DW1000 dw1000(spi, sIRQ, sReset);
 
 
 // main() runs in its own thread in the OS
 int main() {
 	printf("Hello World!\n");
-	// delselect chip (nSS = high)
-	cs = 1;
-	// set up SPI for mode 0
-	spi.format(8, 0);
-	spi.frequency(1000000);
-	
-	// start transmission
-	cs = 0;
-	uint32_t answer = 0;
-	// answer should contain the device id
-	answer +=  spi.write(0x0) << (3*8);
-	answer += spi.write(0x0) << (2*8);
-	answer += spi.write(0x0) << (1*8);
-	answer += spi.write(0x0);
-	cs = 1;
-	printf("%lx\n", (unsigned long int) answer);
+	uint32_t dev_id = dw1000.get_dev_id();
 
-	if(answer != 0) {
+
+	printf("%lx\n", (unsigned long int) dev_id);
+
+	if(dev_id != 0) {
 		led2 != led2;
 	}
 
